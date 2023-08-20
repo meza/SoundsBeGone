4d63f1604f6e855345fc3473f0ca12279a6740b4
Meza <meza@users.noreply.github.com>
2023-08-20 13:49:15 +0100

    fix: reducing the analytics chatter

--- src/client/java/gg/meza/analytics/Analytics.java
+++ src/client/java/gg/meza/analytics/Analytics.java
@@ -6,12 +6,16 @@ import gg.meza.SoundsBeGoneClient;
 import net.minecraft.client.MinecraftClient;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.HashMap;
 import java.util.Map;
+import static gg.meza.SoundsBeGone.LOGGER;
 
 public class Analytics {
-    private static final String POSTHOG_API_KEY = "POSTHOG_API_KEY_REPL";
+//    private static final String POSTHOG_API_KEY = "POSTHOG_API_KEY_REPL";
+    private static final String POSTHOG_API_KEY = "phc_FwrcOlz7iwXh4IlKbFn7yawZ2LiZJ4ew0p5Yy2OoeRSL";
     private static final String POSTHOG_HOST = "https://eu.posthog.com";
-    private PostHog posthog;
+    private final PostHog posthog;
+    private final Map<String, Integer> blockedSoundsCount = new HashMap<>();
 
     // Not actually sending any user info, just using the uuid to create a new uuid that cannot be traced back to the user
     private final String uuid = DigestUtils.sha256Hex(MinecraftClient.getInstance().getSession().getUsername());
@@ -25,13 +29,16 @@ public class Analytics {
             this.sendEvent("Started Minecraft", "");
         }
     }
-
     private void sendEvent(String event, String sound) {
+        this.sendEvent(event, sound, Map.of());
+    }
+
+    private void sendEvent(String event, String sound, Map<String, Object> extraProps) {
         if (!SoundsBeGoneClient.config.isAnalyticsEnabled()) {
             return;
         }
 
-        this.posthog.capture(this.uuid, event, Map.of(
+        Map<String, Object> baseProps = new HashMap<>(Map.of(
                 "sound", sound,
                 "Minecraft Version", MC_VERSION,
                 "OS", OS_NAME,
@@ -39,6 +46,10 @@ public class Analytics {
                 "ModVersion", SoundsBeGone.VERSION,
                 "Java Version", JAVA_VERSION
         ));
+
+        baseProps.putAll(extraProps);
+
+        this.posthog.capture(this.uuid, event, baseProps);
     }
 
     public void mutedSound(String sound) {
@@ -46,10 +57,31 @@ public class Analytics {
     }
 
     public void blockedSound(String sound) {
-        this.sendEvent("Blocked Sound", sound);
+        if (this.blockedSoundsCount.containsKey(sound)) {
+            this.blockedSoundsCount.put(sound, this.blockedSoundsCount.get(sound) + 1);
+        } else {
+            this.blockedSoundsCount.put(sound, 1);
+        }
+
+        if(this.blockedSoundsCount.get(sound) > Integer.MAX_VALUE - 1000) {
+            this.sendEvent("Blocked Sound", sound, Map.of(
+                    "count", this.blockedSoundsCount.get(sound)
+            ));
+            this.blockedSoundsCount.remove(sound);
+        }
     }
 
     public void unmutedSound(String sound) {
         this.sendEvent("UnMuted Sound", sound);
+    }
+
+    public void flush() {
+        LOGGER.debug("Flushing analytics");
+        this.blockedSoundsCount.forEach((sound, count) -> {
+            this.sendEvent("Blocked Sound", sound, Map.of(
+                    "count", count
+            ));
+            this.blockedSoundsCount.remove(sound);
+        });
     }
 }
