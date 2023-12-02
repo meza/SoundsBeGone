6b85b891e8a591f26773f69bf1262f2dfc4e9010
Meza <meza@users.noreply.github.com>
2023-12-02 17:35:19 +0000

    chore: removed the main branch code to reduce confusion

--- src/client/java/gg/meza/ConfigData.java
+++
@@ -1,9 +0,0 @@
-package gg.meza;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class ConfigData {
-    public boolean analytics = true;
-    public Set<String> sounds = new HashSet<>();
-}
