fbd861dd6bbc631a089bfcb9cede57e32fa64b38
Meza <meza@users.noreply.github.com>
2023-12-02 16:53:15 +0000

    feat: added a keyboard shortcut to pull up the config

--- fabric/src/main/java/gg/meza/fabric/SoundsBeGoneFabric.java
+++ fabric/src/main/java/gg/meza/fabric/SoundsBeGoneFabric.java
@@ -6,7 +6,11 @@ import gg.meza.client.SoundsBeGoneClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.*;
 
@@ -18,6 +22,8 @@ public class SoundsBeGoneFabric implements ClientModInitializer {
 	public void onInitializeClient() {
 
 		SoundsBeGoneClient.initClient();
+
+		KeyBindingHelper.registerKeyBinding(SoundsBeGoneClient.openConfig);
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (SoundsBeGoneClient.openConfig.wasPressed()) {
