f7507ffb3b018dc440c5bf32cbe2b5d7fa3b68d5
Meza <meza@users.noreply.github.com>
2023-12-03 14:39:21 +0000

    fix: fixed the copypasta from srs

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,8 +3,8 @@
 VERSION=$1
 
 echo "Replacing version with ${VERSION}"
-sed -e "s/VERSION/${VERSION}/" -i gradle.properties
-sed -e "s/VERSION_REPL/${VERSION}/" -i src/main/java/gg/meza/SoundsBeGone.java
-sed -e "s/POSTHOG_API_KEY_REPL/${POSTHOG_API_KEY}/" -i src/client/java/gg/meza/analytics/Analytics.java
+sed -e "s/0.0-SNAPSHOT/${VERSION}/" -i gradle.properties
+sed -e "s/VERSION_REPL/${VERSION}/" -i common/src/main/java/gg/meza/SoundsBeGone.java
+sed -e "s/POSTHOG_API_KEY_REPL/${POSTHOG_API_KEY}/" -i common/src/client/java/gg/meza/analytics/Analytics.java
 
 ./gradlew build -x test # we've ran the tests earlier in the build
