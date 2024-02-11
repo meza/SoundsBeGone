4d2bd09a9ee5342f002a1e5ea7524ab54c715edf
Meza <meza@users.noreply.github.com>
2024-02-11 09:39:15 +0000

    fix(#10): fixed the cause of a random crash

--- common/src/main/java/gg/meza/client/SoundsBeGoneClient.java
+++ common/src/main/java/gg/meza/client/SoundsBeGoneClient.java
@@ -9,9 +9,10 @@ import org.lwjgl.glfw.GLFW;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SoundsBeGoneClient {
-    public static Map<String, Date> SoundMap = new HashMap<>();
+    public static Map<String, Date> SoundMap = new ConcurrentHashMap<>();
     public static Config config = new Config();
     public static Telemetry telemetry;
     public static final KeyBinding openConfig = new KeyBinding("soundsbegone.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "soundsbegone.keybinds");
