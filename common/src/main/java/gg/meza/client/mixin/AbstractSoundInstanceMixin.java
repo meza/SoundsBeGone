3404a49f77c9c6c7ff13d1ca22da9ea116af9220
Meza <meza@users.noreply.github.com>
2024-03-02 15:51:44 +0000

    fix: minecart, guardian and other sounds are now silenced properly

---
+++ common/src/main/java/gg/meza/client/mixin/AbstractSoundInstanceMixin.java
@@ -0,0 +1,42 @@
+package gg.meza.client.mixin;
+
+import gg.meza.SoundsBeGone;
+import gg.meza.client.SoundsBeGoneClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.AbstractSoundInstance;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractSoundInstance.class)
+public class AbstractSoundInstanceMixin {
+
+    @Final
+    @Shadow
+    protected Identifier id;
+
+    @Inject(
+            method = "getVolume()F",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void getVolume(CallbackInfoReturnable<Float> cir) {
+
+        if (SoundsBeGoneClient.config.isSoundDisabled(id.toString())) {
+            SoundsBeGone.LOGGER.debug("Disabling the sound: {}", id);
+            SoundsBeGoneClient.telemetry.blockedSound(id.toString());
+            cir.setReturnValue(0.0F);
+            cir.cancel();
+        }
+
+        if(MinecraftClient.getInstance().world != null) {
+            SoundsBeGone.LOGGER.debug("Intercepting the sound: {}", Text.translatable(id.toTranslationKey()));
+            SoundsBeGoneClient.SoundMap.put(id.toString(), new java.util.Date());
+        }
+
+    }
+}
