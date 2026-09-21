f75e94a0d5ca36d898942d3fca3e10de0ae54ffc
meza <211977+meza@users.noreply.github.com>
2026-09-21 09:23:32 +0100

    chore: updated stonecraft

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 }
 
 plugins {
-    id("gg.meza.stonecraft") version "1.13.0"
+    id("gg.meza.stonecraft") version "1.13.1"
     id("dev.kikugie.stonecutter") version "0.9+"
 }
 
